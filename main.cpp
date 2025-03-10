41c89833cd0704e5890208cb5c113bdfc55bceda
Mahika Datta <122850772+mahika34@users.noreply.github.com>
2025-03-10 19:55:24 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "This is main program\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout <<"Lab 8 almost done...";
+  cout << "PES2UG22CS292 final";
+  return 0;
+}
